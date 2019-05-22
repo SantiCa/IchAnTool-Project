9100e58ea2f16ffa43db242c2ca7c37a4407286a
SantiCa <48765629+SantiCa@users.noreply.github.com>
2019-05-22 13:07:09 +0200

    acepted

---
+++ Program code/AgeConverter.py
@@ -0,0 +1,12 @@
+"""Conversor de edad para sondeo 977"""
+import pandas as pd
+import os
+from pandas import DataFrame
+from tkinter import filedialog
+import tkinter as tk
+"""____window set_____"""
+window=tk.Toplevel()
+window.title('IchnopaleoCount alfa 1.0')
+window.wm_iconbitmap("IconWindow.ico")
+window.wm_title("Age Converter alfa 1")
+window.attributes("-topmost", True)
